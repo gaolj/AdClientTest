4796fba87ffc639feced0710d93ddd58465252ee
gaolijun <gaolijun@sina.com>
2017-03-06 22:59:02 +0800

    init

---
+++ AdClientTest.cpp
@@ -0,0 +1,18 @@
+// AdClientTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "AdManager.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	AdManager& adManager = AdManager::getInstance();
+	adManager.setConfig("127.0.0.1", 18888, 123456, false, 0);
+	adManager.bgnBusiness();
+	
+	int i;
+	std::cin >> i;
+	
+	return 0;
+}
+
